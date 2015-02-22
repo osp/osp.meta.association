53c49594b00be64ae5a77960e94a142d60831996
Eric Schrijver <eric@ericschrijver.nl>
2015-02-22 14:50:15 +0100

    Also start the Dutch

--- get.sh
+++ get.sh
@@ -4,4 +4,5 @@ curl -O "http://ethertoff.osp.kitchen/raw/roi.md"
 curl -O "http://ethertoff.osp.kitchen/raw/to-do.md"
 curl -O "http://ethertoff.osp.kitchen/raw/style.css"
 curl -O "http://ethertoff.osp.kitchen/raw/registre.md"
+curl -O "http://ethertoff.osp.kitchen/raw/vzw-statutes.md"
 
---
+++ vzw-statutes.md
@@ -0,0 +1,9 @@
+# OSP VZW STATUTEN
+
+De ondergetekende:
+
+- Bram Crevits – BE – Paul Frederiqstraat 80, 9000 Gent
+- Nik Gaffney – AU –  Minimenstraat 60, 1000 Brussel
+- Catherine Lenoble – FR – adresse
+- Femke Snelting – NL – adresse
+- Maxime Lambrecht, ?? – adresse
